cd6d33128c7ba5736ab0b3895952213d4a6b4d18
Danil Struchenckov <danilstru20002203@gmail.com>
2021-09-27 17:27:59 +0300

    Тестовая отправка смс

--- Corporate messenger/Corporate messenger/Models/Chat/ChatModel.cs
+++ Corporate messenger/Corporate messenger/Models/Chat/ChatModel.cs
@@ -1,12 +1,75 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Corporate_messenger.Models.Chat
 {
-    class ChatModel
+    class ChatModel: INotifyPropertyChanged
     {
-        public string Message { get; set; }
-        public string User { get; set; }
+        private int sender_id { get; set; }
+        private string message { get; set; }
+        private int chat_room_id { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
+        [JsonProperty("sender_id")]
+        public int Sender_id
+        {
+            get { return sender_id; }
+            set
+            {
+                if (sender_id != value)
+                {
+                    sender_id = value;
+                    OnPropertyChanged("Sender_id");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
+        [JsonProperty("message")]
+        public string Message
+        {
+            get { return message; }
+            set
+            {
+                if (message != value)
+                {
+                    message = value;
+                    OnPropertyChanged("Message");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
+        [JsonProperty("chat_room_id")]
+        public int Chat_room_id
+        {
+            get { return chat_room_id; }
+            set
+            {
+                if (chat_room_id != value)
+                {
+                    chat_room_id = value;
+                    OnPropertyChanged("Chat_room_id");
+                }
+            }
+        }
     }
 }
--- Corporate messenger/Corporate messenger/ViewModels/ChatViewModel.cs
+++ Corporate messenger/Corporate messenger/ViewModels/ChatViewModel.cs
@@ -1,7 +1,11 @@
 ﻿
+using Corporate_messenger.Models;
+using Corporate_messenger.Models.Chat;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net.Http;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
@@ -18,6 +22,7 @@ namespace Corporate_messenger.ViewModels
         {
             ConnectToServerAsync();
         }
+        SpecialDataModel user = new SpecialDataModel();
         async void ConnectToServerAsync()
         {
             await client.ConnectAsync(new Uri("ws://192.168.0.105:6001/app/ABCDEFG"), CancellationToken.None);
@@ -26,6 +31,34 @@ namespace Corporate_messenger.ViewModels
             var buffer = new ArraySegment<Byte>(encoder, 0, encoder.Length);
             await client.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
             //client.Abort();
+            ChatModel chat_to = new ChatModel();
+            chat_to.Sender_id = user.Id;
+            chat_to.Message = "ss";
+            chat_to.Chat_room_id = 1;
+            // Перед отправкой , превращаем все в json
+            string jsonLog = JsonConvert.SerializeObject(chat_to);
+
+            // Устанавливаем соеденение 
+            HttpClient client2 = new HttpClient();
+
+            // Тип данных который мы принимаем от сервера 
+            var contentType = "application/json";
+
+            // Тип Запроса
+            var httpMethod = HttpMethod.Post;
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri("http://192.168.0.105:8098/api/chat/1"),
+                Method = httpMethod,
+                Content = new StringContent(jsonLog, System.Text.Encoding.UTF8, contentType)
+            };
+            // Отправка заголовка
+            request.Headers.Add("Authorization", "Bearer " + user.Token);
+            // Отправка данных авторизации
+            var httpResponse = await client2.SendAsync(request);
+
+            // Ответ от сервера 
+            var contenJSON = await httpResponse.Content.ReadAsStringAsync();
         }
     }
 }
